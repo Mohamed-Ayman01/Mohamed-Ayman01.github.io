f5370a7683cb37987b0181bf86deea41ca42b579
Mohamed-Ayman01 <lord39597@gmail.com>
2023-07-27 00:13:35 +0400

    finished fetching data from github

--- js/main.js
+++ js/main.js
@@ -54,3 +54,49 @@ menuLinks.forEach((a) => {
     sideMenu.classList.remove("show");
   });
 });
+
+let projectsBox = document.querySelector(".projects .projects-box")
+let myGithubName = "Mohamed-Ayman01";
+
+async function getProjectsData() {
+  let data = await fetch(`https://api.github.com/users/${myGithubName}/repos`);
+
+  if (data.ok === true) {
+    let responseObj = await data.json();
+    let filterdObjs = [];
+
+    for (obj of responseObj) {
+      // ! Until the border-previewer app is solved hide project
+      if (obj.fork === false && obj.homepage !== null && obj.name !== "Mohamed-Ayman01.github.io" && obj.name !== "Border-Radius-Previewer") {
+        filterdObjs.push(obj)
+      }
+    }
+
+    for (obj of filterdObjs) {
+      // console.log(`${obj.homepage}project-img.jpg`);
+      // console.log(obj.homepage);
+
+      let box = document.createElement("div");
+      box.classList.add("box");
+
+      let img = document.createElement("img");
+      img.setAttribute("src", `${obj.homepage}project-img.jpg`);
+
+      let overlay = document.createElement("div");
+      overlay.classList.add("overlay");
+      
+      let viewLink = document.createElement("a");
+      viewLink.textContent = "live view";
+      viewLink.classList.add("view", "main-btn");
+      viewLink.setAttribute("target", "_blank");
+      viewLink.setAttribute("href", obj.homepage);
+      overlay.append(viewLink)
+
+      box.append(img, overlay);
+
+      projectsBox.append(box)
+    }
+  }
+}
+
+getProjectsData()
